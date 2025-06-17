f6e38827fb0d7e04af53fcdcfefa7c475a736aec
xufu2025 <xufu2025@gmail.com>
2025-06-18 00:18:29 +0100

    first step

---
+++ TelemetrySample/src/ConsoleTool/Program.cs
@@ -0,0 +1,19 @@
+﻿await DoWork();
+
+Console.WriteLine("Done!");
+
+static async Task DoWork()
+{
+    await StepOne();
+    await StepTwo();
+}
+
+static async Task StepOne()
+{
+    await Task.Delay(500);
+}
+
+static async Task StepTwo()
+{
+    await Task.Delay(1000);
+}
